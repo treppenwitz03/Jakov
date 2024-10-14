42405d698933ccc13a487d21edcf5d648bd3a894
treppenwitz03 <owenlegrande@gmail.com>
2024-10-14 10:24:15 +0800

    Make mobile dev easier

--- jakov.py
+++ jakov.py
@@ -2,25 +2,29 @@ import flet as ft
 
 import platform
 import os
+import sys
 
 async def main(page: ft.Page):
-    import mobile_ui as ui
-    await ui.start(page)
-    # if platform.system() == "Windows" or platform.system() == "Linux" or platform.system() == "Darwin":
-    #     if "iPhone" in platform.platform():
-    #         import mobile_ui as ui
-    #         await ui.start(page)
-    #         return
-
-    #     import desktop_ui as ui
-    #     await ui.start(page)
-
-    # else:
-    #     if "android" in platform.system().lower() or os.path.isfile('/system/build.prop'):
-    #         import mobile_ui as ui
-    #         await ui.start(page)
-    #         return
+    if platform.system() == "Windows" or platform.system() == "Linux" or platform.system() == "Darwin":
+        if "iPhone" in platform.platform():
+            import mobile_ui as ui
+            await ui.start(page)
+            return
         
-    #     print("Your Operating system is not currently supported by Yakov :<.")
+        if sys.argv[1] == "-m": # TO EASE ANDROID DEVELOPMENT
+            print("MOBILE MODE")
+            import mobile_ui as ui
+            await ui.start(page)
+
+        import desktop_ui as ui
+        await ui.start(page)
+
+    else:
+        if "android" in platform.system().lower() or os.path.isfile('/system/build.prop'):
+            import mobile_ui as ui
+            await ui.start(page)
+            return
+        
+        print("Your Operating system is not currently supported by Yakov :<.")
 
 ft.app(target=main)
